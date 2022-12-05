8c01401b31bfec72aa8b3386cbc96fe5b3bd88a0
Tate <tatebraeckel@gmail.com>
2022-12-04 20:27:44 -0700

    commit tree 112d33d1a047deb48f9b8f528a29535b851da20f

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
-// Code your solution here
+let drivers = ['John', 'Frank', 'Sammy', 'Timmy', 'Simpson'];
+
+
+
+
+
+const matchingNames = function findMatching(names) {
+    matchingNames.filter(drivers, '')
+}
+
+// fuzzyMatch
+
+// matchName
+
+//     `findMatching` - This function takes an array of drivers' names and a `string`
+//   as arguments, and returns the matching list of drivers.The function should be
+//   case insensitive.
+
+// * `fuzzyMatch` - This function takes an array of drivers' names and a `string`
+//   as arguments for querying the array, and returns all drivers whose names begin
+// with the provided letters.
+
+// * `matchName` - This function takes an array of `driver` objects and a`string`
+//   as arguments.Each `driver` object has two properties: `name` and`hometown`.
+//   The function should return each element whose `name` property matches the
+// provided `string` argument.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const drivers = [
+//     {
+//         name: 'Bobby',
+//         hometown: 'Pittsburgh'
+//     },
+//     {
+//         name: 'Sammy',
+//         hometown: 'New York'
+//     },
+//     {
+//         name: 'Sally',
+//         hometown: 'Cleveland'
+//     },
+//     {
+//         name: 'Annette',
+//         hometown: 'Los Angeles'
+//     },
+//     {
+//         name: 'Bobby',
+//         hometown: 'Tampa Bay'
+//     }
+// ];
+
+// function findMatching(drivers, driver) {
+//     return drivers.filter(
+//         (possibleMatch) => possibleMatch.toLowerCase() === driver.toLowerCase()
+//     );
+// }
+
+// function fuzzyMatch(drivers, testString) {
+//     return drivers.filter(
+//         (possibleMatch) => possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     );
+// }
+
+// function matchName(drivers, soughtName) {
+//     return drivers.filter((record) => record.name === soughtName);
+// }
